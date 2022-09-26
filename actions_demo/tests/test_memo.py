9414eef54f30f5e297e358ef85115eb62723965e
hayoon0120 <minyyoon@dob.world>
2022-09-26 16:07:58 +0900

    FEat: Fix test

--- actions_demo/tests/test_memo.py
+++ actions_demo/tests/test_memo.py
@@ -4,10 +4,11 @@ MEMO_URL = "/demo"
 
 class MemoTestCase(TestCase):
 
-    def setUp(self):
-        pass
+    # def setUp(self):
+    #     pass
 
-    def create_memo_success(self):
+    def test_create_memo_success(self):
+        print("TEST START")
         reqeust_body = {"title":"Test Title", "user" : "Test user"}
         response = self.client.post(MEMO_URL, data=reqeust_body)
         result = {
