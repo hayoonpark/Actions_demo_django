3f37775b2400393ae10fbc64239c8a73680aedb7
hayoon0120 <minyyoon@dob.world>
2022-09-26 14:06:48 +0900

    Feat: Fix app test

--- actions_demo/tests/test_memo.py
+++ actions_demo/tests/test_memo.py
@@ -1,9 +1,16 @@
 from django.test import TestCase
 
+MEMO_URL = "/demo"
+
 class MemoTestCase(TestCase):
 
     def setUp(self):
         pass
 
     def create_memo_success(self):
-        pass        
+        reqeust_body = {"title":"Test Title", "user" : "Test user"}
+        response = self.client.post(MEMO_URL, data=reqeust_body)
+        result = {
+            "message" : "success"
+        }
+        self.assertEqual(response.json(), result)
